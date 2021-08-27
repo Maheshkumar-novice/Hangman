b82131a024b725b8ba6ccf3822d23c89f450e35d
Maheshkumar <callmeelprofessor@gmail.com>
2021-08-27 08:57:21 +0530

    Add basic Store Class for file operations

---
+++ lib/store.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Class Store
+class Store
+  def retrieve_file(filename)
+    File.read(filename)
+  end
+
+  def save_file(filename, contents)
+    File.open(filename, 'w') do |file|
+      file.puts contents
+    end
+  end
+end
+
+# Store.new.retrieve_file('lib/user.rb')
+# Store.new.save_file('lib/a.txt', 'hello')

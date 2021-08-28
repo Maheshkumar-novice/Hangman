6e2d445b7a734782ad59e42efd09829e75972d0f
Maheshkumar <callmeelprofessor@gmail.com>
2021-08-28 09:31:04 +0530

    Create first playable version

--- lib/game.rb
+++ lib/game.rb
@@ -10,6 +10,7 @@ require 'pry'
 # Class Game
 class Game
   attr_reader :user, :word, :guess, :result, :file_handler
+  attr_accessor :user_guess
 
   def initialize(user, word, guess, result, file_handler)
     @user = user
@@ -17,37 +18,71 @@ class Game
     @guess = guess
     @result = result
     @file_handler = file_handler
+    @user_guess = nil
   end
 
   def new_game
     word.secret_word = word.generate
-    update_game_state(word.secret_word, [], [], 5)
+    update_game_state(word.secret_word, [], [], 15)
   end
 
   def play
-    word.placeholder = ' _ ' * word.secret_word.size
-    puts word.placeholder
+    word.update_placeholder(guess.correct_guesses)
+    print_result
+    print_remaining_guesses
     loop do
-      user_guess = user.make_guess
-      validation = guess.validate(user_guess, word.secret_word)
-      word.update_placeholder(guess.correct_guesses) if validation
-
-      guess.correct_guesses << user_guess if validation && !guess.correct_guesses.include?(user_guess)
-
-      guess.incorrect_guesses << user_guess if !validation && !guess.incorrect_guesses.include?(user_guess)
-
-      puts word.placeholder
-      print "Correct Guesses: #{guess.correct_guesses}\n"
-      print "Incorrect Guesses: #{guess.incorrect_guesses}\n"
-
-      guess.total_guesses -= 1
-      puts "Guesses Remaining #{guess.total_guesses}"
+      process
       break if guess.total_guesses.zero?
       break if word.placeholder == word.secret_word
     end
   end
 
   private
+
+  def process
+    create_guess
+    validation = validate_guess
+    update_correct_guesses if validation
+    update_placeholder if validation
+    update_incorrect_guesses unless validation
+    print_result
+    update_total_guesses
+    print_remaining_guesses
+  end
+
+  def print_remaining_guesses
+    puts "Guesses Remaining #{guess.total_guesses}"
+  end
+
+  def print_result
+    puts word.placeholder
+    print "Correct Guesses: #{guess.correct_guesses}\n"
+    print "Incorrect Guesses: #{guess.incorrect_guesses}\n"
+  end
+
+  def create_guess
+    self.user_guess = user.make_guess
+  end
+
+  def validate_guess
+    guess.validate(user_guess, word.secret_word)
+  end
+
+  def update_total_guesses
+    guess.total_guesses -= 1
+  end
+
+  def update_placeholder
+    word.update_placeholder(guess.correct_guesses)
+  end
+
+  def update_correct_guesses
+    guess.correct_guesses << user_guess unless guess.correct_guesses.include?(user_guess)
+  end
+
+  def update_incorrect_guesses
+    guess.incorrect_guesses << user_guess unless guess.incorrect_guesses.include?(user_guess)
+  end
 
   def update_game_state(secret_word, correct_guesses, incorrect_guesses, total_guesses)
     word.secret_word = secret_word

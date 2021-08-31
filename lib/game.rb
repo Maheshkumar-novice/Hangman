1c0a22f5115bdcb8e1a442b975cabb94772d9be4
Maheshkumar <callmeelprofessor@gmail.com>
2021-08-31 10:39:41 +0530

    Create hangman.rb

--- lib/game.rb
+++ lib/game.rb
@@ -6,11 +6,13 @@ require_relative 'user'
 require_relative 'word'
 require_relative 'guess'
 require_relative 'modules/game_output'
+require_relative 'modules/hangman'
 require 'yaml'
 
 # Class Game
 class Game
   include GameOutput
+  include Hangman
 
   SAVE_DIR = 'saved_games'
 

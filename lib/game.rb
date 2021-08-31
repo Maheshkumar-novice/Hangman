9c6983df5e8985d41733342ca20dffffd01b45e7
Maheshkumar <callmeelprofessor@gmail.com>
2021-08-31 09:43:42 +0530

    Refactor game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,6 @@ require_relative 'user'
 require_relative 'word'
 require_relative 'guess'
 require_relative 'modules/game_output'
-require 'pry'
 
 # Class Game
 class Game
@@ -23,9 +22,10 @@ class Game
     @guess = guess
     @file_handler = file_handler
     @user_guess = nil
+    @validation = nil
   end
 
-  def intro 
+  def intro
     print_game_intro
   end
 
@@ -39,29 +39,28 @@ class Game
     start if gets.chomp.downcase == 'y'
   end
 
+  private
+
   def new_game
     word.secret_word = word.generate
     word.placeholder = ' _ ' * word.secret_word.size
-    update_game_state(5, [], [], word.secret_word, word.placeholder)
-    play
+    create_game_state(5, [], [], word.secret_word, word.placeholder)
+    play_the_game
   end
 
   def load_game
-    if file_handler.save_not_available?
-      print_no_games_found
-      return
-    else
-      hash = file_handler.game_data
-    end
-    update_game_state(hash[:remaining_incorrect_guesses],
+    return print_no_games_found if file_handler.save_not_available?
+
+    hash = file_handler.game_data
+    create_game_state(hash[:remaining_incorrect_guesses],
                       hash[:correct_guesses],
                       hash[:incorrect_guesses],
                       hash[:secret_word],
                       hash[:placeholder])
-    play
+    play_the_game
   end
 
-  def play
+  def play_the_game
     print_game_state
     loop do
       process_the_game
@@ -70,64 +69,38 @@ class Game
     end
   end
 
-  private
-
-  def save_game
-    filename = file_handler.file_name
-    file_handler.save_file("#{SAVE_DIR}/#{filename}", YAML.dump(create_hash))
-    print_game_saved
-  end
-
-  def save?
-    user_guess == 'save'
-  end
-
-  def exit?
-    user_guess == 'exit'
-  end
-
-  def update_game_state(remaining_incorrect_guesses, correct_guesses, incorrect_guesses, secret_word, placeholder)
-    guess.remaining_incorrect_guesses = remaining_incorrect_guesses
-    guess.correct_guesses = correct_guesses
-    guess.incorrect_guesses = incorrect_guesses
-    word.secret_word = secret_word
-    word.placeholder = placeholder
-  end
-
   def process_the_game
-    create_guess
+    create_user_guess
     return save_game if save?
     return if exit?
 
     if guess.already_guessed?(user_guess)
       print_already_guessed
     else
-      validation = validate_guess
-      update(validation)
+      validation = validate_user_guess
+      update_game_state(validation)
     end
     print_game_state
   end
 
-  def update(validation)
+  def save_game
+    filename = file_handler.file_name
+    file_handler.save_file("#{SAVE_DIR}/#{filename}", YAML.dump(create_game_state_hash))
+    print_game_saved
+  end
+
+  def update_game_state(validation)
     if validation
       update_correct_guesses
       update_placeholder
+      return
     end
 
-    unless validation
-      update_incorrect_guesses
-      update_remaining_incorrect_guesses
-    end
+    update_incorrect_guesses
+    decrease_remaining_incorrect_guesses
   end
 
-  def game_end?
-    return true if guess.remaining_incorrect_guesses.zero?
-    return true if word.placeholder == word.secret_word
-
-    false
-  end
-
-  def create_guess
+  def create_user_guess
     self.user_guess = user.make_guess
     until user_guess =~ /^[a-z]{1}$/i
       break if user_guess =~ /^save$|^exit$/i
@@ -137,27 +110,15 @@ class Game
     end
   end
 
-  def validate_guess
-    guess.validate(user_guess, word.secret_word)
+  def create_game_state(remaining_incorrect_guesses, correct_guesses, incorrect_guesses, secret_word, placeholder)
+    guess.remaining_incorrect_guesses = remaining_incorrect_guesses
+    guess.correct_guesses = correct_guesses
+    guess.incorrect_guesses = incorrect_guesses
+    word.secret_word = secret_word
+    word.placeholder = placeholder
   end
 
-  def update_correct_guesses
-    guess.correct_guesses << user_guess unless guess.correct_guesses.include?(user_guess)
-  end
-
-  def update_placeholder
-    word.update_placeholder(guess.correct_guesses)
-  end
-
-  def update_incorrect_guesses
-    guess.incorrect_guesses << user_guess unless guess.incorrect_guesses.include?(user_guess)
-  end
-
-  def update_remaining_incorrect_guesses
-    guess.remaining_incorrect_guesses -= 1
-  end
-
-  def create_hash
+  def create_game_state_hash
     {
       remaining_incorrect_guesses: guess.remaining_incorrect_guesses,
       correct_guesses: guess.correct_guesses,
@@ -165,5 +126,40 @@ class Game
       secret_word: word.secret_word,
       placeholder: word.placeholder
     }
+  end
+
+  def validate_user_guess
+    guess.validate(user_guess, word.secret_word)
+  end
+
+  def update_placeholder
+    word.update_placeholder(guess.correct_guesses)
+  end
+
+  def update_correct_guesses
+    guess.correct_guesses << user_guess unless guess.correct_guesses.include?(user_guess)
+  end
+
+  def update_incorrect_guesses
+    guess.incorrect_guesses << user_guess unless guess.incorrect_guesses.include?(user_guess)
+  end
+
+  def decrease_remaining_incorrect_guesses
+    guess.remaining_incorrect_guesses -= 1
+  end
+
+  def game_end?
+    return true if guess.remaining_incorrect_guesses.zero?
+    return true if word.placeholder == word.secret_word
+
+    false
+  end
+
+  def save?
+    user_guess == 'save'
+  end
+
+  def exit?
+    user_guess == 'exit'
   end
 end
